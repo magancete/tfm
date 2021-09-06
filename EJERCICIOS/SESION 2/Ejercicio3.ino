e1e44bccb8986d08a3eee1a1fb383bdef17008fb
Carlos Magán <magancete@gmail.com>
2021-09-06 21:04:11 +0200

    Sesión 2

---
+++ EJERCICIOS/SESION 2/Ejercicio3.ino
@@ -0,0 +1,113 @@
+#define PIN_MOTOR_FRONT_LEFT 7
+#define PIN_MOTOR_FRONT_RIGHT 12
+#define PIN_MOTOR_BACK_LEFT 8
+#define PIN_MOTOR_BACK_RIGHT 11
+
+#define PIN_SPEED_RIGHT 3
+#define PIN_SPEED_LEFT 6
+
+int SPEED = 120;
+#define SPEED_LEFT 255
+#define SPEED_RIGHT 255
+#define BACK_SPEED 200
+
+void go_Advance()  //Forward
+{
+  digitalWrite(PIN_MOTOR_FRONT_RIGHT, HIGH);
+  digitalWrite(PIN_MOTOR_BACK_RIGHT,LOW);
+  digitalWrite(PIN_MOTOR_FRONT_LEFT,HIGH);
+  digitalWrite(PIN_MOTOR_BACK_LEFT,LOW);
+  set_Motorspeed(SPEED,SPEED);
+}
+void go_Left()  //Turn left
+{
+  digitalWrite(PIN_MOTOR_FRONT_RIGHT, HIGH);
+  digitalWrite(PIN_MOTOR_BACK_RIGHT,LOW);
+  digitalWrite(PIN_MOTOR_FRONT_LEFT,LOW);
+  digitalWrite(PIN_MOTOR_BACK_LEFT,HIGH);
+  set_Motorspeed(0,SPEED_RIGHT);
+}
+void go_Right()  //Turn right
+{
+  digitalWrite(PIN_MOTOR_FRONT_RIGHT, LOW);
+  digitalWrite(PIN_MOTOR_BACK_RIGHT,HIGH);
+  digitalWrite(PIN_MOTOR_FRONT_LEFT,HIGH);
+  digitalWrite(PIN_MOTOR_BACK_LEFT,LOW);
+  set_Motorspeed(SPEED_LEFT,0);
+}
+void go_Back()  //Reverse
+{
+  digitalWrite(PIN_MOTOR_FRONT_RIGHT, LOW);
+  digitalWrite(PIN_MOTOR_BACK_RIGHT,HIGH);
+  digitalWrite(PIN_MOTOR_FRONT_LEFT,LOW);
+  digitalWrite(PIN_MOTOR_BACK_LEFT,HIGH);
+  set_Motorspeed(BACK_SPEED,BACK_SPEED);
+}
+void stop_Stop()    //Stop
+{
+  digitalWrite(PIN_MOTOR_FRONT_RIGHT, LOW);
+  digitalWrite(PIN_MOTOR_BACK_RIGHT,LOW);
+  digitalWrite(PIN_MOTOR_FRONT_LEFT,LOW);
+  digitalWrite(PIN_MOTOR_BACK_LEFT,LOW);
+  set_Motorspeed(0,0);
+}
+
+void set_Motorspeed(int speed_L,int speed_R) {
+
+  analogWrite(PIN_SPEED_LEFT,speed_L);
+  analogWrite(PIN_SPEED_RIGHT,speed_R);
+
+}
+
+void setup() {
+
+  Serial.begin(9600);
+
+  pinMode(PIN_MOTOR_FRONT_RIGHT, OUTPUT);
+  pinMode(PIN_MOTOR_BACK_RIGHT, OUTPUT);
+  pinMode(PIN_SPEED_RIGHT, OUTPUT);
+
+  pinMode(PIN_MOTOR_FRONT_LEFT, OUTPUT);
+  pinMode(PIN_MOTOR_BACK_LEFT, OUTPUT);
+  pinMode(PIN_SPEED_LEFT, OUTPUT);
+
+  stop_Stop();
+
+}
+
+void loop() {
+
+  if (Serial.available())  {
+
+    char c = Serial.read();
+
+    switch (c){
+      case 'A':
+      go_Advance();
+      break;
+      case 'L':
+      go_Left();
+      break;
+      case 'R':
+      go_Right();
+      break;
+      case 'B':
+      go_Back();
+      break;
+      case 'S':
+      stop_Stop();
+      break;
+      case 'G':
+      SPEED=SPEED+10;
+      set_Motorspeed(SPEED,SPEED);
+      break;
+      case 'J':
+      SPEED=SPEED-10;
+      set_Motorspeed(SPEED,SPEED);
+      break;
+      default:
+      break;
+    }
+
+  }
+}
